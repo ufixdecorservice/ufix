04843c106d08a5448d62ccb24cf7d63ead5d5c72
ufixdecorservice <ufixdecor.service@gmail.com>
2026-06-08 22:41:37 +0700

    Delete Run_Ufix_Server.bat

--- Run_Ufix_Server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ==============================================
-echo  Ufix Decor Service - Local Web Server
-echo  Access your website at: http://localhost:8000
-echo ==============================================
-cd /d "%~dp0"
-python -m http.server --bind 0.0.0.0 8000
-pause
